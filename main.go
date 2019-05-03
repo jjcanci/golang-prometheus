9a8ce993f0370b1f72b8216573eafe0149ba4d24
jjcanci.fvg <juanjose.canci@fravega.com.ar>
2019-05-03 10:35:51 -0300

    cleaning

--- main.go
+++ main.go
@@ -9,32 +9,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/push"
 )
 
-var (
-	completionTime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "db_backup_last_completion_timestamp_seconds",
-		Help: "The timestamp of the last completion of a DB backup, successful or not.",
-	})
-	successTime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "db_backup_last_success_timestamp_seconds",
-		Help: "The timestamp of the last successful completion of a DB backup.",
-	})
-	duration = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "db_backup_duration_seconds",
-		Help: "The duration of the last DB backup in seconds.",
-	})
-	records = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "db_backup_records_processed",
-		Help: "The number of records processed in the last DB backup.",
-	})
-)
-
-func performBackup() (int, error) {
-	// Perform the backup and return the number of backed up records and any
-	// applicable error.
-	// ...
-	return 42, nil
-}
-
 func main() {
 	log.Println("Start")
 	rand.Seed(time.Now().UTC().UnixNano())
